70cc09c308a2ed41d8731616ff764301660c5efd
Amartya Setty <amartya@Amartyas-MacBook-Pro.local>
2021-09-24 13:15:49 -0700

    feat: Add Mr DeVaughn led project

---
+++ angular_12_projects/firestarter/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { SharedModule } from './shared/shared.module';
+import { HomePageComponent } from './home-page/home-page.component';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkDUJfk5wlLm01kMbSlEZjaK_amiRaPhM",
+  authDomain: "firestarter-c360e.firebaseapp.com",
+  projectId: "firestarter-c360e",
+  storageBucket: "firestarter-c360e.appspot.com",
+  messagingSenderId: "542785721701",
+  appId: "1:542785721701:web:09a7d2e76cae33157ebd8c"
+};
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    SharedModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
+  ],
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
